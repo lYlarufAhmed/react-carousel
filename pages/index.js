51220ef87e6b905df84cbef9a0ea0788d6967cbd
pickboy900 <4198957+lYlarufAhmed@users.noreply.github.com>
2022-01-12 12:13:57 +0600

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <div>
+
+            <style jsx global>{`
+              html,
+              body {
+                padding: 0;
+                margin: 0;
+                font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+                Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+                sans-serif;
+              }
+
+              * {
+                box-sizing: border-box;
+                margin: 0;
+                padding: 0;
+              }
+            `}</style>
+        </div>
+    )
+}
